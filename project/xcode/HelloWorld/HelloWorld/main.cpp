35f54f2c3083dfd77cfa17d968db64abe42b9b9f
Christian Holmqvist <christian.holmqvist@king.com>
2020-03-14 19:34:37 +0100

    Initial xcode checkin

---
+++ project/xcode/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Christian Holmqvist on 2020-03-14.
+//  Copyright © 2020 Christian Holmqvist. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    vector<string> msg {"Hello", "World", "from", "XCode", "2020"};
+
+    for ( string word : msg) {
+        cout << word << " ";
+    }
+    cout << endl;
+    return 0;
+}
