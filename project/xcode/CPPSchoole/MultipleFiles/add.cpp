93483efd62d15f52b32159f1fe4d7c103d6b6e32
Christian Holmqvist <christian.holmqvist@king.com>
2020-03-25 08:48:22 +0100

    Played some more!

---
+++ project/xcode/CPPSchoole/MultipleFiles/add.cpp
@@ -0,0 +1,13 @@
+//
+//  add.cpp
+//  MultipleFiles
+//
+//  Created by Christian Holmqvist on 2020-03-24.
+//  Copyright © 2020 Christian Holmqvist. All rights reserved.
+//
+
+#include <stdio.h>
+
+int add(int x, int y) {
+    return x + y;
+}
