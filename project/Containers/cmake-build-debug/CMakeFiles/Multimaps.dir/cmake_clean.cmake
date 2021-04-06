1abb59b943a8748896fa710f481ba5d361a4125d
Christian Holmqvist <christian.holmqvist@king.com>
2021-04-06 10:26:53 +0200

    Changed and updated.

---
+++ project/Containers/cmake-build-debug/CMakeFiles/Multimaps.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Multimaps.dir/multimaps.cpp.o"
+  "Multimaps"
+  "Multimaps.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Multimaps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project/Containers/cmake-build-debug/CMakeFiles/Sets.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sets.dir/sets.cpp.o"
+  "Sets"
+  "Sets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
